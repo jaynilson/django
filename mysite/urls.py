1ab7cd6fa538b4af681c6847ee58b0dae4cc8921
jaynilson <jaynilson@gmail.com>
2024-05-24 00:55:01 -0700

    c0m4

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,6 @@
+
+
+
 """
 URL configuration for mysite project.
 
@@ -15,8 +18,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
+
+
